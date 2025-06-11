8039672be14995275d9f4002caeb8a5ea62e1105
Muhammad Maulana Wijaya <muhammad_wijaya_ts6_23@student.smktelkom-sda.sch.id>
2025-06-11 10:38:51 +0700

    pembaruan pada tailwind.js dan index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,3 +2,13 @@ module.exports = {
   darkMode: 'class', // atau 'media'
   ...
 }
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        superlobster: ['"Super Lobster"', 'cursive'], // Sesuaikan dengan nama font yang diimpor
+      },
+    },
+  },
+};
